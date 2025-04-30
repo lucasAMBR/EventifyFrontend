9fd9322e4de8b1096d7383cf08115016dcbbe411
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-04-29 22:24:56 -0300

    Eventos funcionais e responsividade

--- src/pages/eventSearch/EventSearch.jsx
+++ src/pages/eventSearch/EventSearch.jsx
@@ -1,14 +1,31 @@
 import SearchIcon from '@mui/icons-material/Search';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import api from "../../services/Api";
 
 import style from "./EventSearch.module.css";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { EventCard } from './components/EventCard';
 
 export const EventSearch = () => {
     
     useEffect(() => {
         document.title = "Eventfy - Search"
+
+        fetchPopularEvents();
     }, [])
+
+    const [ allRegisterEvents, setAllRegisterEvents ] = useState([]);
+    const [ popularEvents, setPopularEvents ] = useState([]);
+    const [ recentEvents, setRecentEvents ] = useState([]);
+
+    const [ searchTerm, setSearchTerm ] = useState("");
+
+    const fetchPopularEvents = async() => {
+        const response = await api.get("/event/popular");
+
+        setPopularEvents(response.data)
+        console.log(popularEvents)
+    }
 
     return(
         <>
@@ -21,7 +38,24 @@ export const EventSearch = () => {
                 </div>
             </div>
             <div className={style.event_list_area}>
-                ...
+                <div className={style.popular_events_list}>
+                    <h2 onClick={() => console.log(popularEvents)}>Popular events</h2>
+                    <div className={style.event_list}>
+                        {popularEvents.length == 0 && "Sem eventos cadastrados..."}
+                        {popularEvents.map((item) => 
+                            <EventCard EventBanner={`http://localhost:8080${item.imagePath}`} EventTitle={item.title} EventDate={item.date} EventHour={item.hour} EventLocal={item.location} EventGuestLimit={item.guestLimit} EventHostName={item.organizerName}/>
+                        )}
+                    </div>
+                </div>
+                <div className={style.recent_events_list}>
+                    <h2>Recent events</h2>
+                    <div className={style.event_list}>
+                    {popularEvents.length == 0 && "Sem eventos cadastrados..."}
+                    {popularEvents.map((item) => 
+                        <EventCard EventBanner={`http://localhost:8080${item.imagePath}`} EventTitle={item.title} EventDate={item.date} EventHour={item.hour} EventLocal={item.location} EventGuestLimit={item.guestLimit} EventHostName={item.organizerName}/>
+                    )}
+                    </div>
+                </div>
             </div>
         </>
     );
---
+++ src/pages/eventSearch/components/EventCard.jsx
@@ -0,0 +1,24 @@
+import style from "../EventSearch.module.css";
+
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LocationPinIcon from '@mui/icons-material/LocationPin';
+import PeopleIcon from '@mui/icons-material/People';
+
+export const EventCard = ({EventBanner, EventTitle, EventLocal, EventDate, EventHour, EventGuestLimit, EventHostName}) => {
+
+    return(
+        <div className={style.event_card}>
+            <div className={style.card_image}>
+                <img src={EventBanner} />
+            </div>
+            <div className={style.card_infos}>
+                <p className={style.card_title}>{EventTitle}</p>
+                <p><AccessTimeIcon sx={{fill: '#004643'}} /> {`${EventDate[2]}/${EventDate[1]}/${EventDate[0]}, ${EventHour[0]}:${EventHour[1]}`}</p>
+                <p><LocationPinIcon sx={{fill: '#004643'}} />{EventLocal}</p>
+                <p><PeopleIcon sx={{fill: '#004643'}} />Limit: {EventGuestLimit} person</p>
+                <p><PeopleIcon sx={{fill: '#004643'}} />{EventHostName}</p>
+                <button>More details</button>
+            </div>
+        </div>
+    );
+}
