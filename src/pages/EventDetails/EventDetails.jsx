731f71f15f6764dc23509ae10c1bdd0ec53c539a
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-05-10 19:10:52 -0300

    detalhes quase prontos

--- src/pages/EventDetails/EventDetails.jsx
+++ src/pages/EventDetails/EventDetails.jsx
@@ -35,42 +35,46 @@ export const EventDetails = () => {
         <>
             <div className={style.navbar}>
                 <div className={style.return_button} onClick={() => navigate("/")}><ArrowBackIcon sx={{fill: '#004643'}}/></div>
-                <div className={style.header_logo}><img src="/images/LogoWhite.png" />  -  Details: {"Titulo do evento"}</div>
+                <div className={style.header_logo}><img src="/images/LogoWhite.png" />  -  Details</div>
             </div>
             <div className={style.event_list_area}>
                 <div className={style.event_area}>
-                    <div className={style.event_information}>
-                        <div className={style.event_card}>
-                            <h1>Titulo do evento</h1>
-                            <div className={style.event_image}>
-                                image
+                    {eventData != null &&
+                        <>
+                            <div className={style.event_information}>
+                                <div className={style.event_card}>
+                                    <h1>{eventData.title}</h1>
+                                    <div className={style.event_image}>
+                                        <img src={`http://localhost:8080${eventData.imagePath}`} />
+                                    </div>
+                                    <div className={style.organizer}>
+                                        <div className={style.organizer_profile_pic}></div>
+                                        <div className={style.organizer_name}>{eventData.organizerName}</div>
+                                    </div>
+                                    <div className={style.description}>
+                                        <h2>Description</h2>
+                                        <p>{eventData.description}</p>
+                                    </div>
+                                    <button className={style.subscribe}>Subscribe</button>
+                                </div>
+                                <div className={style.more_infos}>
+                                    <h1>More information</h1>
+                                    <h2><Accessibility sx={{fill: "#004643"}} />Type: {eventData.type}</h2>
+                                    <h2><DateRange sx={{fill: "#004643"}} />Date: {`${eventData.date[2]}/${eventData.date[1]}/${eventData.date[0]}`}</h2>
+                                    <h2><AccessTimeIcon sx={{fill: "#004643"}} />Hour: {`${eventData.hour[0]}:${eventData.hour[1]}`}</h2>
+                                    <h2><LocationPinIcon sx={{fill: "#004643"}} />Local: {`${eventData.location}`}</h2>
+                                    <h2><LocationPinIcon sx={{fill: "#004643"}} />Map: </h2>
+                                    <div className={style.event_image}>image</div>
+                                </div>
                             </div>
-                            <div className={style.organizer}>
-                                <div className={style.organizer_profile_pic}></div>
-                                <div className={style.organizer_name}>Organizer name</div>
+                            <div className={style.divisory}>.</div>
+                            <div className={style.event_information}>
+                                <div className={style.other_events}>
+                                    <h2>Other events from ths organizer</h2>
+                                </div>
                             </div>
-                            <div className={style.description}>
-                                <h2>Description</h2>
-                                <p>testetestetestetestetestetestetestetesteteste testetesteteste testetestetesteteste testetestetesteteste testetestetesteteste</p>
-                            </div>
-                            <button className={style.subscribe}>Subscribe</button>
-                        </div>
-                        <div className={style.more_infos}>
-                            <h1>More information</h1>
-                            <h2><Accessibility sx={{fill: "#004643"}} />Type: PRESENTIAL</h2>
-                            <h2><DateRange sx={{fill: "#004643"}} />Date: 10/10/25</h2>
-                            <h2><AccessTimeIcon sx={{fill: "#004643"}} />Hour: 10:30</h2>
-                            <h2><LocationPinIcon sx={{fill: "#004643"}} />Local: Rua 123, São Paulo, SP</h2>
-                            <h2><LocationPinIcon sx={{fill: "#004643"}} />Map: </h2>
-                            <div className={style.event_image}>image</div>
-                        </div>
-                    </div>
-                    <div className={style.divisory}>.</div>
-                    <div className={style.event_information}>
-                        <div className={style.other_events}>
-                            <h2>Other events from ths organizer</h2>
-                        </div>
-                    </div>
+                        </>
+                    }
                 </div>
             </div>
         </>
