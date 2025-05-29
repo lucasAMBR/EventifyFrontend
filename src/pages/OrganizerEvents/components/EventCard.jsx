44c421ae6764d11e1011219c2a401fe903e93d1c
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-05-29 11:22:35 -0300

    Inicio da criação do modal de edição de eventos

--- src/pages/OrganizerEvents/components/EventCard.jsx
+++ src/pages/OrganizerEvents/components/EventCard.jsx
@@ -1,11 +1,13 @@
+import { Description } from "@mui/icons-material";
 import style from "../Events.module.css";
 
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import LocationPinIcon from '@mui/icons-material/LocationPin';
 import PeopleIcon from '@mui/icons-material/People';
+import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-export const EventCard = ({EventId, EventBanner, EventTitle, EventLocal, EventDate, EventHour, EventGuestLimit, EventHostName}) => {
+export const EventCard = ({EventId, EventBanner, EventTitle, EventLocal, EventDate, EventHour, EventGuestLimit}) => {
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -13,6 +15,14 @@ export const EventCard = ({EventId, EventBanner, EventTitle, EventLocal, EventDa
     const handleOpenEventDetails = ( id ) => {
         navigate(`/event-search/details/${id}`, {state: {from: location.pathname}});
     };
+
+    const [ updateModalIsOpen, setUpdateModalIsOpen ] = useState(false);
+    const [ choosedEvent, setChoosedEvent ] = useState()
+
+    const handleOpenModal = (id) => {
+        setChoosedEvent(id);
+        setUpdateModalIsOpen(true);
+    }
 
     return(
         <div className={style.event_card}>
@@ -25,7 +35,7 @@ export const EventCard = ({EventId, EventBanner, EventTitle, EventLocal, EventDa
                 <p><LocationPinIcon sx={{fill: '#004643'}} />{EventLocal}</p>
                 <p><PeopleIcon sx={{fill: '#004643'}} />Limit: {EventGuestLimit} person</p>
                 <button onClick={() => handleOpenEventDetails(EventId)}>View event feed</button>
-                <button onClick={() => handleOpenEventDetails(EventId)}>Edit event data</button>
+                <button onClick={() => handleOpenModal(EventId)}>Edit event data</button>
             </div>
         </div>
     );
---
+++ src/pages/OrganizerEvents/components/UpdateEventModal.jsx
@@ -0,0 +1,10 @@
+import style from "../Events.mobile.css"
+
+const UpdateEventModal = ({EventId, EventTitle, EventLocal, EventDate, EventHour, EventGuestLimit}) => {
+
+    return(
+        <div className={style.add_event}>
+            Teste
+        </div>
+    )
+}
