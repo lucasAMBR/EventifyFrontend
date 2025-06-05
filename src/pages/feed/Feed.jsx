615a8174515670d40ce1f2e51bc656deaa524a78
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-06-04 23:33:41 -0300

    Area de adição de post pronta

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -5,6 +5,7 @@ import { useUserContext } from "../../contexts/UserContext"
 import { useNavigate } from "react-router-dom";
 
 import { PostItem } from "./components/PostItem";
+import { ImageSelector } from "./components/ImageSelector";
 
 export const Feed = () => {
 
@@ -61,22 +62,22 @@ export const Feed = () => {
         <div className={style.feed_area}>
             <div className={style.post_list}>
                 <div className={style.add_post_form}>
-                    <h2 onClick={() => console.log(userPopularFeed)}>Make a post</h2>
                     <form>
+                        <div className={style.form_header}>
+                            <h2 onClick={() => console.log(userPopularFeed)}>Make a post</h2>
+                             <ImageSelector />
+                        </div>
+                
                         <textarea placeholder="Insert the post content here"></textarea>
                         <div className={style.post_buttons_area}>
-                            <div className={style.image_input}>
-                                <p>Select the post images (optional) (limit 3 images per post)</p>
-                                <input type="file"></input>
-                            </div>
                             <button type="submit" className={style.publish_button}>Publish</button>
                         </div>
                     </form> 
                 </div>
                 <div className={style.feed_selector}>
                     <span className={selectedFeed == "popular" ? style.selected : ""} onClick={() => handleFeedSwitch("popular")}>Popular</span> | <span className={selectedFeed == "following" ? style.selected : ""} onClick={() => handleFeedSwitch("following")}>Following</span>
-                </div>
                 <div className={style.feed_post_list}>
+                </div>
                     {!loading && userPopularFeed.length > 0 && selectedFeed == "popular" &&
                         <>
                             {userPopularFeed.map((post, index) => (
---
+++ src/pages/feed/components/ImageSelector.jsx
@@ -0,0 +1,25 @@
+import style from "../Feed.module.css";
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import { useRef } from "react";
+
+export const ImageSelector = ({onfilesSelected, max = 3}) => {
+
+    const fileInputRef = useRef(null);
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (event) => {
+        const files = Array.from(event.target.files).slice(0, max);
+        onfilesSelected(files);
+    };
+
+    return(
+        <div className={style.file_selector_button}>
+            <button className={style.image_selector_button} type="button" onClick={handleButtonClick}><AddPhotoAlternateIcon sx={{fill: "#004643"}} /></button>
+
+            <input type="file" accept="image/*" multiple ref={fileInputRef} onChange={handleFileChange} hidden/>
+        </div>
+    )
+}
